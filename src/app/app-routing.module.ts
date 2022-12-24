d6549d434a4f97f50cf0fd21e56215407bd2a809
Pablo Cappellacci <pablocappe@gmail.com>
2022-12-23 23:49:35 -0300

    paths and mock up - meet 12-24-22

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CustomersComponent } from './Components/customers/customers.component';
+import { HomeComponent } from './Components/home/home.component';
+import { TodaysCardComponent } from './Components/todays-card/todays-card.component';
+
+
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'customers', component: CustomersComponent},
+  {path: 'tcards', component: TodaysCardComponent},
+  {path: '**', pathMatch:'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
