97fe7719d07e95c589a404bd236cf8598ba49b5a
Pablo Cappellacci <pablocappe@gmail.com>
2023-05-17 06:03:19 -0300

    Fix Errors deploy

--- src/app/Components/customers/customers.component.ts
+++ src/app/Components/customers/customers.component.ts
@@ -22,8 +22,7 @@ export class CustomersComponent implements OnInit {
   customer: Customer;
   customers: any [];
   customerPerPage: number;
-  totalItems:number;
-  totalPages:number;
+
 
   constructor(
               private service: CustomerService) { }
@@ -32,7 +31,6 @@ export class CustomersComponent implements OnInit {
 
     await this.getAllCustomers()
 
-    this.customerPerPage, this.totalItems = this.getAllCustomers.length
   }
 
   
@@ -92,41 +90,5 @@ export class CustomersComponent implements OnInit {
 
 
   }
-
-  // async deleteCustomer(customer: Customer) {
-  //   const result = await Swal.fire({
-  //     title: 'Are you really sure about it?',
-  //     text: "You won't be able to undo the action",
-  //     icon: 'warning',
-  //     showCancelButton: true,
-  //     confirmButtonColor: '#3085d6',
-  //     cancelButtonColor: '#d33',
-  //     confirmButtonText: 'Yes, Delete',
-  //     cancelButtonText: 'Cancel'
-  //   });
-  
-  //   if (result.isConfirmed) {
-  //     const res = await this.customerService.deleteRecord(customer.id);
-  //     if (this.okDelete(customer.id, this.customers)) {
-  //       await Swal.fire(
-  //         'Deleted',
-  //         'The customer was delete successfully.',
-  //         'success'
-  //       );
-  //       this.getCustomers()
-  //     } else {
-  //       await Swal.fire(
-  //         'Error',
-  //         'Error, please try again',
-  //         'error'
-  //       );  
-  //     }
-  //   }
- 
-  // }
-
-  // okDelete(idSearch, customers){
-  //  return customers.find(customer => customer.id === idSearch);
-  // }
 
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'birthday-reminder-4310c',
-    appId: '1:691632785024:web:dd693a0eafaec2d2d7c865',
-    storageBucket: 'birthday-reminder-4310c.appspot.com',
-    apiKey: 'AIzaSyCUL9YY5hRbFwQXPUxLGMph6icjuhnHi_o',
-    authDomain: 'birthday-reminder-4310c.firebaseapp.com',
-    messagingSenderId: '691632785024',
-    measurementId: 'G-DS3JG47EJD',
+    apiKey: "AIzaSyBjYYFOzlQxe3HrH9LjrLw_GhdvOHtBFFg",
+    authDomain: "portfolioprod-576cd.firebaseapp.com",
+    projectId: "portfolioprod-576cd",
+    storageBucket: "portfolioprod-576cd.appspot.com",
+    messagingSenderId: "1041585006791",
+    appId: "1:1041585006791:web:b5067a38a423b4fc38b6e2",
+    measurementId: "G-44T8WLK1FP"
   },
     production: false,
   };
